0cf2cf57a7ca4568d39495e904110ee30c3f6b96
Skobart <bskorowski@gmail.com>
2020-03-08 16:36:28 +0100

    update1

--- src/app/PointApplication.java
+++ src/app/PointApplication.java
@@ -11,19 +11,29 @@ public class PointApplication {
         Point p1 = new Point(10, 20);
         PointController pc = new PointController();
 
-        System.out.println("Punkt: (" + p1.getX() + ";" + p1.getY() + ")");
-        pc.addX(p1);
-        System.out.println("Punkt addX: (" + p1.getX() + ";" + p1.getY() + ")");
-        pc.addY(p1);
-        pc.addY(p1);
-        pc.addY(p1);
-        pc.addY(p1);
-        System.out.println("Punkt addY: (" + p1.getX() + ";" + p1.getY() + ")");
-        pc.minusX(p1);
-        System.out.println("Punkt minusX: (" + p1.getX() + ";" + p1.getY() + ")");
-        pc.minusY(p1);
-        System.out.println("Punkt minusY: (" + p1.getX() + ";" + p1.getY() + ")");
-        pc.changePoint(p1);
-        System.out.println("Punkt changePoint: (" + p1.getX() + ";" + p1.getY() + ")");
+        final int addX = 0;
+        final int addY = 1;
+        final int minusX = 2;
+        final int minusY = 3;
+
+        int option = addX;
+        switch (option) {
+            case addX:
+                pc.addX(p1);
+                break;
+            case addY:
+                pc.addY(p1);
+                break;
+            case minusX:
+                pc.minusX(p1);
+                break;
+            case minusY:
+                pc.minusY(p1);
+                break;
+            default:
+                System.out.println("Podano błędną wartość");
+        }
+
+        System.out.println("Punkt po zmianie: (" + p1.getX() + ";" + p1.getY() + ")");
     }
 }
